4d14f428a9a2b27a02ad3ee69cb2e754803d37e4
PoutineQc <chagnon.s21@gmail.com>
2017-02-22 14:32:42 -0500

    Added comments for Node and Path

--- GraphMaker/src/ca/polymtl/inf2810/lab1/Node.java
+++ GraphMaker/src/ca/polymtl/inf2810/lab1/Node.java
@@ -3,58 +3,117 @@ package ca.polymtl.inf2810.lab1;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A Node represents a vertice in a Graph. It has a name, a value and a type. It
+ * can be linked to other Nodes in the Graph via Arcs.
+ * 
+ * @author Sébastien Chagnon (1804702)
+ *
+ */
 public class Node {
 	private String name;
 	private NodeType type;
 	private Integer value;
 	private Map<Node, Integer> edges;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param name
+	 *            the name of the Node
+	 * @param type
+	 *            the type of the Node
+	 * @param value
+	 *            the value of the Node
+	 */
 	public Node(String name, NodeType type, Integer value) {
 		this.name = name;
 		this.type = type;
 		this.value = value;
-		
+
 		this.edges = new HashMap<Node, Integer>();
-		
+
 	}
 
+	/**
+	 * Sets the distance between this Node and another one.
+	 * 
+	 * @param node
+	 *            the Node to create a edge to
+	 * @param distance
+	 *            the distance of the edge
+	 */
 	public void setDistance(Node node, Integer distance) {
 		this.edges.put(node, distance);
 	}
-	
+
+	/**
+	 * Checks if this Node is directly connected to another Node
+	 * 
+	 * @param node
+	 *            the Node to check the connection to
+	 * @return true if they are connected, false otherwise
+	 */
 	public boolean isConnectedTo(Node node) {
 		return edges.containsKey(node);
 	}
 
+	/**
+	 * Returns the value of the Node
+	 * 
+	 * @return the value of the Node
+	 */
 	public Integer getValue() {
 		return value;
 	}
 
+	/**
+	 * Returns the type of the Node
+	 * 
+	 * @return the type of the Node
+	 */
 	public NodeType getType() {
 		return type;
 	}
 
-	public enum NodeType {
-		RIEN, POKEMON, POKESTOP, ARENE
-	}
-
+	/**
+	 * Returns the distance between this Node and another one
+	 * 
+	 * @param node
+	 *            the other Node
+	 * @return the distance between the two nodes
+	 */
 	public Integer getDistanceFrom(Node node) {
 		return edges.get(node);
 	}
 
-	@Override
-	public String toString() {
-		return type.name() + " " + name + "(" + value + ", " + getDeactivationTime() + ")";
-	}
-
+	/**
+	 * Returns a map of Nodes and Integer representing the edges between this
+	 * Node and the other Nodes
+	 * 
+	 * @return a map of Nodes and Integer
+	 */
 	public Map<Node, Integer> getConnections() {
 		return edges;
 	}
 
+	/**
+	 * Checks if this name equals the String passed in parameter.
+	 * 
+	 * @param name
+	 *            the name to check
+	 * @return true if it's the same name, false otherwise
+	 */
 	public boolean hasName(String name) {
 		return this.name.equals(name);
 	}
 
+	/**
+	 * Returns the deactivation time of the Node, based on it's type and it's
+	 * value it it's a Pokemon.
+	 * 
+	 * @return the deactivation time
+	 */
 	public Integer getDeactivationTime() {
 		switch (type) {
 		case ARENE:
@@ -77,8 +136,23 @@ public class Node {
 		}
 	}
 
+	/**
+	 * Returns the name of the Node
+	 * 
+	 * @return the name of the Node
+	 */
 	public String getName() {
 		return name;
+	}
+
+	/**
+	 * The possible types of Nodes
+	 * 
+	 * @author Sébastien Chagnon (1804702)
+	 *
+	 */
+	public enum NodeType {
+		RIEN, POKEMON, POKESTOP, ARENE
 	}
 
 }
--- GraphMaker/src/ca/polymtl/inf2810/lab1/Path.java
+++ GraphMaker/src/ca/polymtl/inf2810/lab1/Path.java
@@ -8,6 +8,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * A Path represents a route between many Nodes in a Graph via Arcs
+ * 
+ * @author Sébastien Chagnon (1804702)
+ *
+ */
 public class Path implements Comparable<Path> {
 
 	Graph graph;
@@ -16,6 +22,13 @@ public class Path implements Comparable<Path> {
 	LinkedList<Node> pastNodes;
 	Map<Node, Integer> deactivated;
 
+	/**
+	 * Constructor from a graph. The path is initiallised with one past Node
+	 * which is the start point.
+	 * 
+	 * @param graph
+	 *            the graph to start a path from
+	 */
 	public Path(Graph graph) {
 		this.graph = graph;
 		this.pastNodes = new LinkedList<Node>();
@@ -26,47 +39,94 @@ public class Path implements Comparable<Path> {
 		this.pastNodes.add(graph.getStartPoint());
 	}
 
+	/**
+	 * Constructor that extends another path by adding a node to it's route
+	 * 
+	 * @param path
+	 *            the Path to extends
+	 * @param newNode
+	 *            the new Node to add to the Path
+	 * @param graph
+	 *            the graph in which the Path is
+	 */
 	public Path(Path path, Node newNode, Graph graph) {
 
 		this.graph = graph;
 
+		// Update the distance traveled
 		int lastTravel = path.pastNodes.getLast().getDistanceFrom(newNode);
 		this.distanceTraveled = path.distanceTraveled + lastTravel;
 
+		// Copy the past Nodes from the original Path and add the new Node to
+		// the end of it
 		this.pastNodes = new LinkedList<Node>(path.pastNodes);
 		this.pastNodes.addLast(newNode);
 
+		// Copy the deactivated Nodes from the original Path
 		this.deactivated = new HashMap<>(path.deactivated);
+
+		// A list of Nodes to remove from the disabled map
 		List<Node> toReactivate = new ArrayList<Node>();
+
+		// for each Entry in the deactivated map
 		for (Entry<Node, Integer> vertice : this.deactivated.entrySet()) {
 
+			// The new distance cooldown is calculated
 			int updatedDistance = vertice.getValue() - lastTravel;
+
+			// If the cooldown is over, add the Node to the reactivate list
 			if (updatedDistance <= 0)
 				toReactivate.add(vertice.getKey());
+
+			// Else update the cooldown
 			else
 				vertice.setValue(updatedDistance);
 		}
 
+		// Remove all the Node which their cooldowns are over from the
+		// deactivated list
 		for (Node vertice : toReactivate)
 			this.deactivated.remove(vertice);
 
+		// Update the amount of points gained
 		this.pointsGained = this.deactivated.containsKey(newNode) ? path.pointsGained
 				: path.pointsGained + newNode.getValue();
 
+		// Deactivate the arrival Node
 		this.deactivated.put(newNode, newNode.getDeactivationTime());
 	}
 
-	public List<Path> getSubPaths(RatioAlgorithm dijkstra) {
+	/**
+	 * Get all the subpaths that can be continued from this Path
+	 * 
+	 * @param algorithm
+	 * @return a List of the subpaths
+	 */
+	public List<Path> getSubPaths(RatioAlgorithm algorithm) {
+		// Initialize a new list that will contain the subPaths
 		List<Path> subPaths = new ArrayList<Path>();
 
+		// for each connections from the last Node of the Path to other Nodes
 		for (Entry<Node, Integer> edge : pastNodes.getLast().getConnections().entrySet()) {
+
+			// Creates a new subPath
 			Path newSubPath = new Path(this, edge.getKey(), this.graph);
 
-			if (dijkstra.isFinished(newSubPath)) {
-				dijkstra.addFinishedPath(newSubPath);
-			} else if (dijkstra.isTooFar(newSubPath)) {
-				dijkstra.addFinishedPath(this);
+			if (algorithm.isFinished(newSubPath)) {
+
+				// If the new subPath is finished, add it to the finished paths
+				// of the algorithm
+				algorithm.addFinishedPath(newSubPath);
+
+			} else if (algorithm.isTooFar(newSubPath)) {
+
+				// If the new subPath is to far, add this path to the finished
+				// paths of the algoritm
+				algorithm.addFinishedPath(this);
+
 			} else {
+
+				// Else add the new subPath to the subPath List
 				subPaths.add(newSubPath);
 			}
 		}
@@ -74,6 +134,12 @@ public class Path implements Comparable<Path> {
 		return subPaths;
 	}
 
+	/**
+	 * Get the ratio distance over points of this Path. If the amount of points
+	 * is 0, returns the maximum value of a Double.
+	 * 
+	 * @return
+	 */
 	public double getRatio() {
 		double ratio;
 		try {
@@ -85,39 +151,27 @@ public class Path implements Comparable<Path> {
 		return ratio;
 	}
 
+	/**
+	 * Returns the total amount of points from the Path
+	 * 
+	 * @return the total amount of points from the Path
+	 */
 	public int getTotalPoints() {
 		return pointsGained;
 	}
 
+	/**
+	 * Returns the total distance travelled on this Path
+	 * 
+	 * @return the total distance travelled on this Path
+	 */
 	public int getDistance() {
 		return distanceTraveled;
 	}
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-
-		Iterator<Node> it = pastNodes.iterator();
-		int distance = 0;
-		int points = 0;
-
-		Node node = it.next();
-		sb.append("\n - " + String.format("%1$-18s", node));
-		while (it.hasNext()) {
-			Node previousNode = node;
-			node = it.next();
-			sb.append("\n - " + String.format("%1$-18s", node));
-
-			distance += previousNode.getDistanceFrom(node);
-			points += node.getValue();
-
-			sb.append("Ratio = " + distance + "/" + points + " = " + ((double) distance / points));
-
-		}
-
-		return sb.toString();
-	}
-
+	/**
+	 * Compare the Path based on their ratios.
+	 */
 	@Override
 	public int compareTo(Path o) {
 
@@ -131,29 +185,55 @@ public class Path implements Comparable<Path> {
 		return 0;
 	}
 
+	/**
+	 * To get a list of all the steps of the Path, with the corresponding points
+	 * and distances
+	 * 
+	 * @return an Array of String that can be put in a JList to explain a Graph
+	 */
 	public String[] getDetailedRoute() {
+		// Initialize the list with the right size
 		String[] detailedRoute = new String[pastNodes.size()];
 
+		// Iterator to iterate through the Path's Nodes
 		Iterator<Node> it = pastNodes.iterator();
+
+		// Current step number
 		int i = 0;
+
+		// Current Node
 		Node node = it.next();
+
+		// Temporary path to simulate the Path from it's beginning and get the
+		// Points and Distances from every steps of the way
 		Path temp = new Path(graph);
 
+		// Add the information of the start point
 		detailedRoute[i++] = String.format("%1$-8s", node.getName()) + "Distance:"
-				+ String.format("%1$-6s", temp.distanceTraveled) + "Points:" + String.format("%1$-6s", temp.pointsGained);
+				+ String.format("%1$-6s", temp.distanceTraveled) + "Points:"
+				+ String.format("%1$-6s", temp.pointsGained);
 
+		// While iterator is not at the end of the list
 		while (it.hasNext()) {
+			
+			// Add the current Node to the temporary Path
 			node = it.next();
-
 			temp = new Path(temp, node, graph);
 
+			// Add the information of the current Node and the Paths Distance and Points
 			detailedRoute[i++] = String.format("%1$-8s", node.getName()) + "Distance:"
-					+ String.format("%1$-6s", temp.distanceTraveled) + "Points:" + String.format("%1$-6s", temp.pointsGained);
+					+ String.format("%1$-6s", temp.distanceTraveled) + "Points:"
+					+ String.format("%1$-6s", temp.pointsGained);
 		}
 
 		return detailedRoute;
 	}
 
+	/**
+	 * Returns the List of Nodes this Path passes through
+	 * 
+	 * @return the List of Nodes this Path passes through
+	 */
 	public List<Node> getVertices() {
 		return pastNodes;
 	}
